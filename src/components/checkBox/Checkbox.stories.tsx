e8ec400fe132958b7469c278cb0421e997405209
Abdur Rahaman <abdur@hashdhronas.con>
2021-08-17 02:10:29 +0600

    finish checkbox component

--- src/components/checkBox/Checkbox.stories.tsx
+++ src/components/checkBox/Checkbox.stories.tsx
@@ -4,7 +4,11 @@ import { Checkbox } from "./Checkbox";
 export default {
   title: "Example/CheckBox",
   component: Checkbox,
-  argTypes: {},
+  argTypes: {
+    boxSide: {
+      control: { type: "radio", options: ["start", "end"] },
+    },
+  },
 } as ComponentMeta<typeof Checkbox>;
 
 const Template: ComponentStory<typeof Checkbox> = (args) => (
@@ -14,4 +18,25 @@ const Template: ComponentStory<typeof Checkbox> = (args) => (
 export const Basic = Template.bind({});
 Basic.args = {
   label: "Basic",
+  checked: false,
+  indeterminate: false,
+  boxSide: "start",
+};
+
+export const Checked = Template.bind({});
+Checked.args = {
+  label: "Checked",
+  checked: true,
+};
+
+export const Indeterminate = Template.bind({});
+Indeterminate.args = {
+  label: "Indeterminate",
+  indeterminate: true,
+};
+
+export const BoxSide = Template.bind({});
+BoxSide.args = {
+  label: "Box Side",
+  boxSide: "start",
 };
